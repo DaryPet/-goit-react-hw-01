11310b3df1d5adb3e0c59dc0a619b8a9bc0ab959
DaryPet <d7akkord@gmail.com>
2024-03-28 13:18:17 +0100

    clsx for FirendsCard

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -1,8 +1,13 @@
 import styles from "./FriendListItem.module.css";
+import clsx from "clsx";
 
 export default function FriendListItem({
   friend: { avatar, name, isOnline, id },
 }) {
+  const statusClassNames = clsx(
+    styles.frienStatus,
+    isOnline ? styles.isOnline : styles.isOffline
+  );
   return (
     <div className={styles.card}>
       <img
@@ -12,13 +17,7 @@ export default function FriendListItem({
         width="48"
       />
       <p className={styles.friendName}>{name}</p>
-      <p className={styles.friendStatus}>
-        {isOnline ? (
-          <p className={styles.online}>Online</p>
-        ) : (
-          <p className={styles.offline}>Offline</p>
-        )}
-      </p>
+      <p className={statusClassNames}> {isOnline ? "Online" : "Offline"}</p>
     </div>
   );
 }
