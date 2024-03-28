11840c9c91342de28940bfe6e7057437fab0c9e4
DaryPet <d7akkord@gmail.com>
2024-03-28 09:46:48 +0100

    transactions-jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,15 @@ import Profile from "../Profile/Profile";
 import FriendList from "../Friends/FriendList";
 import userData from "../userData.json";
 import friends from "../friends.json";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import items from "../ transactions.json";
 
 export default function App() {
   return (
     <>
       <Profile data={userData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={items} />
     </>
   );
 }
